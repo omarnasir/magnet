443de03b59b823f5dc23c4a9b0ba0bde1a489f19
omarnasir <momar.nasir@gmail.com>
2017-11-12 18:37:35 +0200

    garbage

--- js/script.js
+++ js/script.js
@@ -1,34 +1,61 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-    count = 0;
-    if (window.DeviceOrientationEvent) {
-        window.addEventListener('deviceorientation', function (eventData) {
-            var isTrue = eventData.absolute;
-            if(isTrue == 1)
-                count++;
-            // gamma: Tilting the device from left to right. Tilting the device to the right will result in a positive value.
-            var tiltLR = eventData.gamma;
+	count = 0;
 
-            // beta: Tilting the device from the front to the back. Tilting the device to the front will result in a positive value.
-            var tiltFB = eventData.beta;
+	if ('ondeviceorientationabsolute' in window) {
+		document.getElementById("supported").innerHTML = true;
+		window.addEventListener('deviceorientationabsolute', function (eventData) {
+			var heading = 0;
+			if (eventData.absolute === true && eventData.alpha !== null) {
+				heading = compassHeading(eventData.alpha, eventData.beta, eventData.gamma);
+				// Call the function to use the data on the page.
+			}
+			deviceOrientationHandler(heading);
+		})
+	}
+	else if ('ondeviceorientation' in window) {
+		document.getElementById("supported").innerHTML = false;
+	}
 
-            // alpha: The direction the compass of the device aims to in degrees.
-            var dir = eventData.alpha
+	function compassHeading(alpha, beta, gamma) {
 
-            // Call the function to use the data on the page.
-            deviceOrientationHandler(count, tiltLR, tiltFB, dir);
-        }, false);
-    };
+		// Convert degrees to radians
+		var alphaRad = alpha * (Math.PI / 180);
+		var betaRad = beta * (Math.PI / 180);
+		var gammaRad = gamma * (Math.PI / 180);
 
-    function deviceOrientationHandler(count, tiltLR, tiltFB, dir) {
-        document.getElementById("isTrue").innerHTML = Math.ceil(count);
-        document.getElementById("tiltLR").innerHTML = Math.ceil(tiltLR);
-        document.getElementById("tiltFB").innerHTML = Math.ceil(tiltFB);
-        document.getElementById("direction").innerHTML = Math.ceil(dir);
+		// Calculate equation components
+		var cA = Math.cos(alphaRad);
+		var sA = Math.sin(alphaRad);
+		var cB = Math.cos(betaRad);
+		var sB = Math.sin(betaRad);
+		var cG = Math.cos(gammaRad);
+		var sG = Math.sin(gammaRad);
 
-        // Rotate the disc of the compass.
-        var compassDisc = document.getElementById("compassDiscImg");
-        compassDisc.style.webkitTransform = "rotate(" + dir + "deg)";
-        compassDisc.style.MozTransform = "rotate(" + dir + "deg)";
-        compassDisc.style.transform = "rotate(" + dir + "deg)";
-    }
+		// Calculate A, B, C rotation components
+		var rA = - cA * sG - sA * sB * cG;
+		var rB = - sA * sG + cA * sB * cG;
+		var rC = - cB * cG;
+
+		// Calculate compass heading
+		var compassHeading = Math.atan(rA / rB);
+
+		// Convert from half unit circle to whole unit circle
+		if (rB < 0) {
+			compassHeading += Math.PI;
+		} else if (rA < 0) {
+			compassHeading += 2 * Math.PI;
+		}
+
+		// Convert radians to degrees
+		compassHeading *= 180 / Math.PI;
+
+		return compassHeading;
+
+	}
+
+	function deviceOrientationHandler(res, alpha) {
+		document.getElementById("heading").innerHTML = Math.ceil(res);
+
+	}
 });
+
